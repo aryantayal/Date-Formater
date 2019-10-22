8ba711b8aad625baa243f60001d78febe5cc610e
Aryan Tayal <atayal99@gmail.com>
2019-10-22 14:09:22 -0500

    y

---
+++ src/Proj5.java
@@ -0,0 +1,21 @@
+ /* This program scrambles a word from the list and then makes a unscramble game for a person to play
+  *
+  *
+  * Aryan Tayal
+  * Project 4
+  * Thurs 2:30
+  */
+
+
+ import java.io.File;
+ import java.io.IOException;
+ import java.util.Arrays;
+ import java.util.Random;
+ import java.util.Scanner;
+
+ public class Proj5 {
+    public static void main(String[] args ) throws IOException {
+        Scanner s = new Scanner(System.in);
+        Scanner inFile = new Scanner(new File("words56.txt"));
+    }
+}
