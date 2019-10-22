45b1786ed681860b3c64cdb7b4cf3a5b26535b85
Aryan Tayal <atayal99@gmail.com>
2019-10-22 14:18:27 -0500

    y

--- src/Proj5.java
+++ src/Proj5.java
@@ -8,6 +8,8 @@
 
 
  import java.io.File;
+ import java.io.FileInputStream;
+ import java.io.FileOutputStream;
  import java.io.IOException;
  import java.util.Arrays;
  import java.util.Random;
@@ -15,7 +17,20 @@
 
  public class Proj5 {
     public static void main(String[] args ) throws IOException {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
         Scanner s = new Scanner(System.in);
-        Scanner inFile = new Scanner(new File("words56.txt"));
+        System.out.print("Enter name of input file: ");
+        String inputFile = s.nextLine();
+        in = new FileInputStream(inputFile);
+
+        System.out.print("Enter name of output file: ");
+        String outputFile = s.nextLine();
+        out = new FileOutputStream(outputFile);
+
+
+
+        //Scanner inFile = new Scanner();
     }
 }
