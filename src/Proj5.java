12d012a5357e8c066d31dc7b4d578ab20ff1519c
Aryan Tayal <atayal99@gmail.com>
2019-10-23 13:04:08 -0500

    y

--- src/Proj5.java
+++ src/Proj5.java
@@ -12,25 +12,59 @@
  import java.io.FileOutputStream;
  import java.io.IOException;
  import java.util.Arrays;
- import java.util.Random;
  import java.util.Scanner;
 
  public class Proj5 {
-    public static void main(String[] args ) throws IOException {
-        FileInputStream in = null;
-        FileOutputStream out = null;
+     public static void main(String[] args) throws IOException {
+         FileInputStream in = null;
+         FileOutputStream out = null;
 
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter name of input file: ");
-        String inputFile = s.nextLine();
-        in = new FileInputStream(inputFile);
+         Scanner s = new Scanner(System.in);
+         System.out.print("Enter name of input file: ");
+         String inputFile = s.nextLine();
+         in = new FileInputStream(inputFile);
 
-        System.out.print("Enter name of output file: ");
-        String outputFile = s.nextLine();
-        out = new FileOutputStream(outputFile);
+         System.out.print("Enter name of output file: ");
+         String outputFile = s.nextLine();
+         out = new FileOutputStream(outputFile);
 
+         Scanner inFile = new Scanner(new File(inputFile));
 
+         String [] tokens = inputFile.split("/");
 
-        //Scanner inFile = new Scanner();
-    }
-}
+         while (inFile.hasNext()) {
+             for(int i = 0; i< tokens.length; i++){
+                System.out.print(tokens[i]);
+             }
+
+         }
+
+         boolean loop;
+
+         //do {
+             System.out.print("Choose a date format type:");
+             System.out.print("\n\t(1) DD/MM/YYYY (ex. 26/08/2019, with leading zeroes on months and days)");
+             System.out.print("\n\t(2) DD Mon, YYYY (ex. 26 Aug, 2019, with leading zeroes on days)");
+             System.out.print("\n\t(3) DOW, Month DDD, YYYY (ex. Monday, August 26, 2019, no leading zeroes)");
+             System.out.print("\n\t(4) Julian format, YYYYddd (where dd is day from 001-365)\n" +
+                     "Enter an option, 1-4:");
+            /* int choice = s.nextLine(0);
+             if (choice = 1) {
+
+             }
+             if (choice = 2) {
+
+             }
+             if (choice = 3) {
+
+             }
+             if (choice = 4) {
+
+             } else {
+
+             }
+         //} while (loop);
+*/
+         //public static int
+     }
+ }
